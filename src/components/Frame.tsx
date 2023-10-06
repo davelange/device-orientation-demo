1a03e1680d115e8783e1a79c565333434705c70a
David Lange <lange@finiam.com>
2023-10-06 18:52:45 +0100

    add perspective and blur effects

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, type CSSProperties } from "react";
 
 const DEBOUNCE_INTERVAL = 10;
 const DAMPING = 10;
@@ -6,6 +6,7 @@ const DAMPING = 10;
 export default function Frame() {
   const [debug, setDebug] = useState();
   const [bgImageShift, setBgImageShift] = useState(0);
+  const [fgImageRotate, setFgImageRotate] = useState(0);
 
   const ref = useRef<ReturnType<typeof setTimeout>>(0);
   const enabled = useRef(true);
@@ -42,22 +43,51 @@ export default function Frame() {
     debounce();
 
     setBgImageShift(event.gamma / DAMPING);
+    setFgImageRotate((event.gamma / 6) * -1);
   }
 
   return (
     <div className="frame">
-      <img
-        className="frame__image"
-        src="/bg_1.webp"
-        alt="A very impressive bridge"
-        style={{ translate: `${bgImageShift}% 0` }}
-      />
+      <div
+        style={{
+          height: "110%",
+          transform: `translateX(${bgImageShift}%) perspective(200px) rotateY(${fgImageRotate}deg)`,
+        }}
+      >
+        <img
+          className="frame__image"
+          src="/bg_1.webp"
+          alt="A very impressive bridge"
+        />
+        <img
+          className="frame__image--blurred"
+          src="/bg_1.webp"
+          alt="A very impressive bridge"
+          style={
+            {
+              opacity: Math.abs(bgImageShift) > 1.9 ? 1 : 0,
+              "-webkit-mask-image":
+                bgImageShift > 0
+                  ? `linear-gradient(90deg, black 40%, transparent 60%)`
+                  : `linear-gradient(90deg, transparent 40%, black 60%)`,
+            } as CSSProperties
+          }
+        />
+      </div>
+      {/* <div className="frame__blur"></div> */}
       <img
         className="frame__logo"
         src="/logo-offblack.svg"
         alt="A very impressive logo"
+        style={{
+          transform: `perspective(200px) rotateY(${
+            fgImageRotate * 2
+          }deg) translateX(${bgImageShift * 3}%)`,
+        }}
       />
-      <p style={{ position: "absolute" }}>{bgImageShift.toPrecision(3)}</p>
+      {/* <p style={{ position: "absolute", top: 0 }}>
+        {bgImageShift.toPrecision(3)}
+      </p> */}
     </div>
   );
 }
